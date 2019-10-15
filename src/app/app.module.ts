e4e6f1f1e3324fee8b4d43b55058b5c6cfad9086
Cutter72 <drelich_pawel@o2.pl>
2019-10-15 19:20:44 +0200

    Handle form input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { NewEstateOfferFormComponent } from './new-estate-offer-form/new-estate-offer-form.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { NewEstateOfferFormComponent } from './new-estate-offer-form/new-estate-
     NewEstateOfferFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/new-estate-offer-form/new-estate-offer-form.component.ts
+++ src/app/new-estate-offer-form/new-estate-offer-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-new-estate-offer',
@@ -6,10 +7,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-estate-offer-form.component.css']
 })
 export class NewEstateOfferFormComponent implements OnInit {
+  newEstateOffer: NewEstateOffer;
+  @ViewChild('f', {static: true}) signedForm: NgForm;
 
-  constructor() { }
+  constructor() {
+    this.newEstateOffer = new NewEstateOffer();
+  }
 
   ngOnInit() {
   }
 
+  onSubmit() {
+    if (this.signedForm.valid) {
+      this.newEstateOffer.city = this.signedForm.value.city;
+      this.newEstateOffer.street = this.signedForm.value.street;
+      this.newEstateOffer.property = this.signedForm.value.property;
+      this.newEstateOffer.apartment = this.signedForm.value.apartment;
+      this.newEstateOffer.price = this.signedForm.value.price;
+      this.newEstateOffer.type = this.signedForm.value.type;
+      this.newEstateOffer.description = this.signedForm.value.description;
+      console.log(this.newEstateOffer);
+    }
+  }
 }
+
+class NewEstateOffer {
+  city: string;
+  street: string;
+  property: string;
+  apartment: string;
+  price: number;
+  type: number;
+  description: string;
+
+  constructor() {
+  }
+}
+
