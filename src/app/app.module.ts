ac5e640913026829d0a6004f17ff02452d7895bd
Cutter72 <drelich_pawel@o2.pl>
2019-10-15 15:19:24 +0200

    newEstateOffer template created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { NewEstateOfferComponent } from './new-estate-offer/new-estate-offer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    NewEstateOfferComponent
   ],
   imports: [
     BrowserModule
