ff0d8d4298b1217245fc228138a357fc38138c25
Cutter72 <drelich_pawel@o2.pl>
2019-10-16 08:52:29 +0200

    get all offers work

--- src/app/my-estate-offers-list/my-estate-offers-list.component.ts
+++ src/app/my-estate-offers-list/my-estate-offers-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {AppComponent} from '../app.component';
 
 @Component({
   selector: 'app-my-estate-offers-list',
@@ -6,10 +8,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-estate-offers-list.component.css']
 })
 export class MyEstateOffersListComponent implements OnInit {
-
-  constructor() { }
+  estateOffers: Array<EstateOffer>;
+  constructor(private http: HttpClient) {
+    this.estateOffers = new Array<EstateOffer>();
+  }
 
   ngOnInit() {
   }
 
+  getAllEstateOffers() {
+    this.http.get(AppComponent.serverAddress + '/getAll').subscribe(responseData => {
+      this.estateOffers = responseData.data;
+      console.log(responseData);
+      console.log(this.estateOffers);
+    });
+  }
+
+}
+class EstateOffer {
+  id: number;
+  city: string;
+  street: string;
+  property: string;
+  apartment: string;
+  price: number;
+  type: number;
+  description: string;
+
+  constructor() {
+  }
 }
