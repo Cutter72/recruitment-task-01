32ef2344ff582992bb66a4c5507eef85e72b59dc
Cutter72 <drelich_pawel@o2.pl>
2019-10-16 18:39:08 +0200

    refesh offers list  onInit

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,18 +7,8 @@ import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   collapsed = false;
-  @Output() newEstateOfferFormViewToggle = new EventEmitter();
-  @Output() myEstateOffersViewToggle = new EventEmitter();
   constructor() { }
 
   ngOnInit() {
-  }
-
-  toggleViewNewEstateOfferForm() {
-    this.newEstateOfferFormViewToggle.emit();
-  }
-
-  toggleViewMyEstateOffers() {
-    this.myEstateOffersViewToggle.emit();
   }
 }
--- src/app/my-estate-offers-list/my-estate-offers-list.component.ts
+++ src/app/my-estate-offers-list/my-estate-offers-list.component.ts
@@ -16,6 +16,7 @@ export class MyEstateOffersListComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.getAllEstateOffers();
   }
 
   getAllEstateOffers() {
